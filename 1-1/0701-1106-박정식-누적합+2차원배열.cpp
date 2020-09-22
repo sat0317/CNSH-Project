5f128575d3ce2b12077ed14439eabfbd44c9ca3e
Yangsa <sat0317@naver.com>
2020-09-22 13:09:40 +0900

    Create 0701-1106-박정식-누적합+2차원배열.cpp

---
+++ 1-1/0701-1106-박정식-누적합+2차원배열.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n, a[11][11]={0, }, m[11][11]={0, }, i, j;
+    scanf("%d", &n);
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            scanf("%d", &a[i][j]);
+            m[i][j] = a[i][j]+m[i][j-1]+m[i-1][j]-m[i-1][j-1];//m[i][j-1]과 m[i-1][j]의 공통 부분은 m[i-1][j-1]임; 공통부분 제거 
+        }
+    }
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            printf("%d\t", m[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}

5184e226da233ae1273c073298548824afaf629e
Yangsa <sat0317@naver.com>
2020-09-29 07:42:01 +0900

    Create 1106-박정식-1712.cpp

---
+++ 1-1/1106-박정식-1712.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n, arr[100][100], cnt=0;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            arr[n-j-1][i]=++cnt;
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}

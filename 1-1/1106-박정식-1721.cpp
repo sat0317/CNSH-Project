9d09c024d8a2952b31467e225c2abfd4f2e09117
Yangsa <sat0317@naver.com>
2020-10-04 13:32:28 +0900

    Create 1106-박정식-1721.cpp

---
+++ 1-1/1106-박정식-1721.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	int n, i, m, cnt=0, j, ar[100][100];
+	scanf("%d %d", &n, &m);
+	for(i=0;i<n;i++){
+		if(!(i%2)){
+			for(j=0;j<m;j++){
+				ar[n-i-1][m-j-1]=++cnt;
+			}
+		}
+		else {
+			for(j=m;j>0;j--){
+				ar[n-i-1][m-j]=++cnt;
+			}
+		}
+	}
+	
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			printf("%d ", ar[i][j]);
+		}
+		printf("\n"); 
+	}
+	
+	return 0;	
+}

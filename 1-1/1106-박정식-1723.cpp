f16e28de8601527e8464c474ffb9368529656add
Yangsa <sat0317@naver.com>
2020-10-05 23:31:02 +0900

    Create 1106-박정식-1723.cpp

---
+++ 1-1/1106-박정식-1723.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    int n, m, sum=1;
+    int array[101][101]={};
+    scanf("%d %d", &m, &n);
+    for(int i=0;i<n;i++){
+        if(!(i%2)){
+            for(int j=0;j<m;j++){
+               array[i][j]=sum++;
+            }
+        } else {
+            for(int j=m-1; j>=0; j--){
+               array[i][j]=sum++;
+            }
+        }
+    }
+    for(int i=m-1;i>=0;i--){
+        for(int j=n-1;j>=0;j--){
+      	    printf("%d\t", array[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}

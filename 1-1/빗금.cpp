50a1cd130d1d77a7e473c10d61247843519278f6
Yangsa <sat0317@naver.com>
2020-10-14 08:15:08 +0900

    Create 빗금.cpp

---
+++ 1-1/빗금.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int i, j, cnt=0, a[100][100]={0, }, n, m;
+	scanf("%d %d", &n, &m);
+	
+	for(i=0;i<n+m;i++){
+		for(j=0;j<=i;j++){
+			if(i-j<m&&i-j>=0&&j>=0&&j<n)
+			cnt+=1;
+			a[j][i-j] = cnt;
+		}
+	}
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}

61677e451704aea6c40f3a760819d8848346fdc2
Yangsa <sat0317@naver.com>
2020-11-01 09:17:54 +0900

    Create 0916-1106-PJS-Vector.cpp

---
+++ 1-2/0916-1106-PJS-Vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector<int> v;
+    cout<<"[v[i], v.size(), v.capacity()]"<<endl;
+    
+    for(int i=0;i<=10;i++){
+        v.push_back(i+1);
+        cout<<"["<<v[i]<<", "<<v.size()<<", "<<v.capacity()<<"]"<<endl;
+    }
+    
+    return 0;
+}

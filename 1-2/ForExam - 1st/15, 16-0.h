ebb9cbeb0a10fe8a58b79096f481875e65ff1b0e
Yangsa <sat0317@naver.com>
2020-11-17 08:33:50 +0900

    Update 15, 16-0.h

--- 1-2/ForExam - 1st/15, 16-0.h
+++ 1-2/ForExam - 1st/15, 16-0.h
@@ -1,6 +1,19 @@
 #ifndef SORT_H_INCLUDED
 #define SORT_H_INCLUDED
 
+void bubble_sort(int arr[], int n){
+	int i, j, t;
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-i;j++){
+			if(arr[j-1]>arr[j]){
+				t=arr[j-1];
+				arr[j-1]=arr[j];
+				arr[j]=t;
+			}
+		}
+	}
+}
+
 void selection_sort(int arr[], int n){
 	int i, j, min, t;
 	for(i=0;i<n-1;i++){

121f11b76f21125c0d0d2f90ae990c547f20f235
Yangsa <sat0317@naver.com>
2020-11-19 10:34:59 +0900

    Create !!default.cpp

---
+++ 2020ComputingChallenge/!!default.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    
+    
+    return 0;
+}
+

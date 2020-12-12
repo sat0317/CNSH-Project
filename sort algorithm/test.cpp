14afbbb76b41d2312dc33398302ee10a7bcf182c
Yangsa <sat0317@naver.com>
2020-12-13 02:43:13 +0900

    Create test.cpp

---
+++ sort algorithm/test.cpp
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+int main(){
+	int n, a[100];
+	scanf("%d", &n);
+	for(int i=1;i<=n;i++){
+		scanf("%d", &a[i]);
+	}
+	
+	_____Sort(___________);
+	
+	for(int i=1;i<=n;i++){
+		printf("%d ", a[i]);
+	}
+	
+	return 0;
+}

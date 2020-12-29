880487839c1eaabbbe0fd51e179eae60e617ff4d
Yangsa <sat0317@naver.com>
2020-12-29 23:27:19 +0900

    Create 2회차 B.cpp

---
+++ 2020ComputingChallenge/2회차 B.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+	int m, y;
+    scanf("%d %d", &y, &m);
+    switch(m){
+    	case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+    		printf("31");
+    		break;
+    	case 4: case 6: case 9: case 11:
+    		printf("30");
+    		break;
+    	case 2:
+    		if(y%4==0&&(y%100!=0||(y%100==0&&y%400==0)))
+    			printf("29");
+    		else printf("28");
+	}
+    
+    return 0;
+}
+

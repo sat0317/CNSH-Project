c5c0b43279146935aa30bdc4486ab79803a07bba
Yangsa <sat0317@naver.com>
2021-01-28 02:06:43 +0900

    Create 7회차 C-2.cpp

---
+++ 2020ComputingChallenge/7회차 C-2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int x[100], y[100], map[100][100]={}, n, ans=0, imsi;
+	int xm=0, ym=0, xM=0, yM=0;
+    
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+    	scanf("%d %d", &x[i], &y[i]);
+    	map[x[i]][y[i]]=1;
+    	if(i){
+    		
+		}
+	}
+	
+	
+	return 0;
+}

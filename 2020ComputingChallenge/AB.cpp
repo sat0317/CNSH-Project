0e08d6f9f4addc4c1aad1417432d81bfba8e91e4
Yangsa <sat0317@naver.com>
2021-02-12 02:47:51 +0900

    Create AB.cpp

---
+++ 2020ComputingChallenge/AB.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a==(!b));
+    
+    return 0;
+}
+

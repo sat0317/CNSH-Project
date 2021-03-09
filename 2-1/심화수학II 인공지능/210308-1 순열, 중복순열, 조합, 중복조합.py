184c7ddddc74d07ecfee02c3b27258b362c42922
Yangsa <sat0317@naver.com>
2021-03-09 13:55:22 +0900

    Update 210308-1 순열, 중복순열, 조합, 중복조합.py

--- 2-1/심화수학II 인공지능/210308-1 순열, 중복순열, 조합, 중복조합.py
+++ 2-1/심화수학II 인공지능/210308-1 순열, 중복순열, 조합, 중복조합.py
@@ -4,7 +4,7 @@
 경우의수 itertools
 
 순서 o 중복 x -> 순열 -> permutations -> 순열 계산 가능
-순서 o 중복 o -> 순열 -> product -> 중순 계산 가능 // 이 부분은 선생님도 햇갈리심. 명확한 건 검색 필요.
+순서 o 중복 o -> 중복순열 -> product -> 중순 계산 가능
 순서 x 중복 x -> 조합 -> combinations
 순서 x 중복 o -> 중복조합 -> combinations_with_replacement
 '''
@@ -15,7 +15,7 @@ a = [1, 2, 3]
 
 ans1=list(permutations(a, 2))
 ans2=list(combinations(a, 2))
-ans3=list(product(a))
+ans3=list(product(a, a))
 ans4=list(combinations_with_replacement(a, 2))
 
 print(len(ans1), " : ", ans1)

d639f6ba2cc84017a517f606a85696d27f0d6655
Yangsa <sat0317@naver.com>
2021-03-09 14:04:52 +0900

    Create 210309-2 자료형.py

---
+++ 2-1/심화수학II 인공지능/210309-2 자료형.py
@@ -0,0 +1,12 @@
+print(type(3)) #integer -> int
+print(type(3.1)) #real number -> float
+print(type("문자열"), "\n") #string -> str, python에는 char이 없음
+
+print(1+1) #덧셈
+print(1-1) #뺄셈
+print(3*2) #곱셈
+print(2**4) #제곱
+print(2^5) #XOR
+print(2/4) #나눗셈
+print(2//4) #몫, 나머지 없는 나눗셈
+print(5%3) #나머지

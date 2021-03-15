c4eeca98297c19d94574f9cbf510da3741455261
Yangsa <sat0317@naver.com>
2021-03-15 13:50:56 +0900

    Create 210315-1 list.py

---
+++ 2-1/심화수학II 인공지능/210315-1 list.py
@@ -0,0 +1,16 @@
+'''
+A = [1, 2, 3, 4, 5, 6]
+B = list(range(1, 7))
+C = np.arange(1, 7)
+'''
+import numpy as np
+from itertools import *
+A = [1, 2, 3, 4, 5, 6]
+B = list(range(1, 7))
+C = np.arange(1, 7)
+D = range(1, 7)
+
+print(type(A), " : ", A)
+print(type(B), " : ", B)
+print(type(C), " : ", C)
+print(type(D), " : ", D)

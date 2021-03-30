f59203ade3d3a4f4b19f7f4394ac56db8ee1d325
Yangsa <sat0317@naver.com>
2021-03-30 14:01:23 +0900

    Create 210330-1 순열.py

---
+++ 2-1/심화수학II 파이썬/210330-1 순열.py
@@ -0,0 +1,23 @@
+'''
+문자 A , B, C, D , E가 하나씩 적혀 있는 장의 카드와
+숫자  ,  ,  , 가 하나씩 적혀 있는 장의 카드가 있다.
+이 장의 카드를 모두 한 번씩 사용하여 일렬로 임의로 나열할 때,
+문자 A 가 적혀 있는 카드의 바로 양옆에 각각 숫자가 적혀 있는
+카드가 놓일 확률은?
+'''
+
+from itertools import *
+from fractions import Fraction as frac
+total = ['A', 'B', 'C', 'D', 'E', 1, 2, 3, 4]
+A = list(permutations(total))
+print(len(A))
+ans = []
+for i in A:
+    a  = i.index('A')
+    if a!=0 and a!=8:
+        if (i[a-1]) in [1, 2, 3, 4] and (i[a+1]) in [1, 2, 3, 4]:
+            ans.append(i)
+print(len(ans))
+print(frac(len(ans), len(A)))
+#print(ans)
+

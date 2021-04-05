16094bcee48af6322ae541cce188567ab779fa6d
Yangsa <sat0317@naver.com>
2021-04-06 02:16:25 +0900

    Create 210405-1 모자배분(중복조합).py

---
+++ 2-1/심화수학II 파이썬/210405-1 모자배분(중복조합).py
@@ -0,0 +1,13 @@
+'''
+4명의 학생 A, B, C, D에게 검은색 모자 6개와
+흰색 모자 6개를 나누어줌
+- 각 학생은 1개 이상 바음
+- 학생 A가 받는 검은색 모자 개수는 4개 이상 -> 얘 기준으로 경우 나누기
+- 흰색 모자보다 검은색 모자를 더 많이 받는 학생은 A를 포함하여 2명뿐
+'''
+
+from itertools import combinations_with_replacement as cwr
+
+BB = ['Ab', 'Bb', 'Cb', 'Db'] #검은모자 받는 개수 -> 중복조합시켜서 개수 세기
+bGet = list(cwr(BB, 6));
+#print(len(bGet))

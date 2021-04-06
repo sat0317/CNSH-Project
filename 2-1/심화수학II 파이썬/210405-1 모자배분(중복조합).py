302b2d69da756e511c999432106d51ceccaa9da9
Yangsa <sat0317@naver.com>
2021-04-06 14:13:29 +0900

    Update 210405-1 모자배분(중복조합).py

--- 2-1/심화수학II 파이썬/210405-1 모자배분(중복조합).py
+++ 2-1/심화수학II 파이썬/210405-1 모자배분(중복조합).py
@@ -6,8 +6,22 @@
 - 흰색 모자보다 검은색 모자를 더 많이 받는 학생은 A를 포함하여 2명뿐
 '''
 
-from itertools import combinations_with_replacement as cwr
+from itertools import combinations_with_replacement as H
+from itertools import product as P
 
+'''
 BB = ['Ab', 'Bb', 'Cb', 'Db'] #검은모자 받는 개수 -> 중복조합시켜서 개수 세기
-bGet = list(cwr(BB, 6));
+bGet = list(H(BB, 6));
 #print(len(bGet))
+'''
+
+BB = ['Ab', 'Bb', 'Cb', 'Db']
+WW = ['Aw', 'Bw', 'Cw', 'Dw']
+
+BBbc = list(H(BB, 6)) #4H6
+WWwc = list(H(WW, 6)) #4H6
+#두 집합의 곱: product
+
+total = list(P(BBbc, WWwc))
+WB = [i[0]+i[1] for i in total]
+print(len(WB))

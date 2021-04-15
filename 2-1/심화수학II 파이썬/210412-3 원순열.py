651f54230f3ed77aed6d7fb3e450bdd127ff492c
Yangsa <sat0317@naver.com>
2021-04-15 17:07:45 +0900

    Update 210412-3 원순열.py

--- 2-1/심화수학II 파이썬/210412-3 원순열.py
+++ 2-1/심화수학II 파이썬/210412-3 원순열.py
@@ -8,3 +8,14 @@
 6C3을 하고 3!으로 앉히기, 2!으로 AB 자리바꾸기
 '''
 
+from itertools import permutations as p
+total=['A','B','C','D','E','F','G','H']
+case=list(p(total[2:],3))
+won=[i+('A','B') for i in case]+[i+('B','A') for i in case]
+print(len(won))
+
+
+Student=[1, 2, 3, 4, 5, 6, 7, 8]
+All_case=list(p(Student, 5))
+Case=[i for i in All_case if i[0] == 1 and (i[1] == 2 or i[4] == 2)]
+print(len(Case))

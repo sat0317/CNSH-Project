e2cedec85599e67cd6720505b1817ba01916ed88
Yangsa <sat0317@naver.com>
2021-04-15 13:15:13 +0900

    Create 부동소수점.cpp

---
+++ 2-1/부동소수점.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	if(0.1*0.1==0.01){
+		printf("1. True\n");
+	}
+	if(0.5*0.5==0.25){
+		printf("2. True\n");
+	}
+	if(0.1+0.2==0.3){
+		printf("3. True\n");
+	} 
+	return 0;
+}

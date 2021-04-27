e398cc68b781a31d1877f2de7bc0cbbc250f9001
Yangsa <sat0317@naver.com>
2021-04-28 08:19:51 +0900

    Create 1119-1106-박정식-BFS-file.cpp

---
+++ 1-2/1119-1106-박정식-BFS-file.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+using namespace std;
+
+FILE *in=fopen("in.txt", "r");
+FILE *out=fopen("out.txt", "w"); 
+int n, s, g[100][100], visited[100];
+int q[100], rear, front=1;
+
+int q_empty(){
+    if(rear<front) return 1;
+    else return 0;
+}
+
+void q_push(int s){
+    q[++rear]=s;
+}
+
+void q_pop(){
+    front++;
+}
+
+int q_top(){
+    return q[front];
+}
+
+void BFS(){
+    q_push(s);
+    visited[s]=1;
+    while(!q_empty()){
+        s=q_top();
+        fprintf(out, "%d ", s);
+        q_pop();
+        for(int i=1;i<=n;i++){
+            if(g[s][i]&&!visited[i]){
+                q_push(i);
+                visited[i]=1;
+            }
+        }
+    }
+}
+
+
+int main(){
+    int x, y;
+    fscanf(in, "%d %d", &n, &s);
+    
+    while(fscanf(in, "%d %d", &x, &y)!=EOF){
+        g[s][e]=g[e][s]=1;
+    }
+    
+    BFS();
+    
+    fclose(in);
+    fclose(out);
+    
+    return 0;
+}

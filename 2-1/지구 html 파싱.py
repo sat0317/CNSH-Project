5941171b3236d331b590d6f28e2ab07efb141f33
Yangsa <sat0317@naver.com>
2021-05-31 16:48:29 +0900

    Create 지구 html 파싱.py

---
+++ 2-1/지구 html 파싱.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup as bs
+from pprint import pprint
+import requests
+html = requests.get('https://news.daum.net/')
+
+soup = bs(html.text,'html.parser')
+print('DAUM NEWS\n')
+data1 = soup.findAll('a', {'class':'link_txt'})
+for i in data1[0:24]:
+    print(i.text)

21a7c9b852ae6059df4440aed2d2ebfd703fe7d1
Yangsa <sat0317@naver.com>
2021-10-12 17:57:44 +0900

    Create BM.cpp

---
+++ 2020 ComputingChallenge/BM.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int a[100][100], n, cnt=0;
+    scanf("%d", &n);
+    
+    	for(int j=0;j<n;j++){
+    for(int i=0;i<n;i++){
+    		if(j%2==0) a[i][j]=++cnt;
+    		else a[n-i-1][j]=++cnt;
+		}
+	}
+    
+    for(int i=0;i<n;i++){
+    	for(int j=0;j<n;j++){
+    		printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+    
+    
+    
+    return 0;
+}
+

506199110323818a6d86bd3266b01f16ff5eeb47
Yangsa <sat0317@naver.com>
2021-10-13 11:10:31 +0900

    Create BQ.cpp

---
+++ 2020 ComputingChallenge/BQ.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int a[100][100], n, m, cnt=0;
+    scanf("%d %d", &n, &m);
+    
+    	for(int j=0;j<m;j++){
+    for(int i=0;i<n;i++){
+    		if(j%2) a[i][m-j-1]=++cnt;
+    		else a[n-i-1][m-j-1]=++cnt;
+		}
+	}
+    
+    for(int i=0;i<n;i++){
+    	for(int j=0;j<m;j++){
+    		printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+    
+    
+    
+    return 0;
+}
+

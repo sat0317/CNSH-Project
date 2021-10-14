1e18c514d0b7086d2f955e8b219eb73eede10cb0
Yangsa <sat0317@naver.com>
2021-10-15 08:06:57 +0900

    Create BV.cpp

---
+++ 2020 ComputingChallenge/BV.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int i, j, cnt=0, a[100][100]={0, }, n, m;
+	scanf("%d %d", &n, &m);
+	
+	for(i=0;i<n+m;i++){
+		for(j=0;j<=i;j++){
+			if(i-j<n&&i-j>=0&&j>=0&&j<m)
+			a[i-j][j] = ++cnt;
+		}
+	}
+	for(i=0;i<n;i++){
+		for(j=m-1;j>=0;j--){
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}

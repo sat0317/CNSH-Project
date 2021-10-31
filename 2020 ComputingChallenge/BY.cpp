eecb8c053f605c468a1ab843634cf0021a236722
Yangsa <sat0317@naver.com>
2021-10-31 13:24:47 +0900

    Create BY.cpp

---
+++ 2020 ComputingChallenge/BY.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int i, j, cnt=0, a[100][100]={0, }, n, m;
+	scanf("%d %d", &n, &m);
+	
+	for(i=0;i<n+m;i++){
+		for(j=0;j<=i;j++){
+			if(i-j<m&&i-j>=0&&j>=0&&j<n)
+			a[j][i-j] = ++cnt;
+		}
+	}
+	for(i=n-1;i>=0;i--){
+		for(j=0;j<m;j++){
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}

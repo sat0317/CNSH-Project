b36c58175fe56aa120cbd474814b509b90b802a7
Yangsa <sat0317@naver.com>
2021-11-01 17:19:38 +0900

    Create C.cpp

---
+++ 2020 ComputingChallenge/C.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    double a;
+    scanf("%lf", &a);
+    printf("%.2lf", a);
+    
+    return 0;
+}
+

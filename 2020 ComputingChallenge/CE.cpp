8e3917229aa7533a9d20fdca1b6e7990dbfe1492
Yangsa <sat0317@naver.com>
2021-11-12 12:21:51 +0900

    Create CE.cpp

---
+++ 2020 ComputingChallenge/CE.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+	int n, m, cnt=0, arr[100][100];
+void thgs(int i, int j){
+	if(arr[i][j]==0) arr[i][j]=++cnt;
+	
+}
+int main(){
+	int thx=-1, thy=0, mo=1, ti=0;
+	scanf("%d %d", &n, &m);
+	
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m-i;j++){
+			thx+=mo;
+			thgs(thy, thx);
+		}
+		for(int j=0;j<n-i-1;j++){
+			thy+=mo;
+			thgs(thy, thx);
+		}
+		mo*=-1;
+	}
+	
+	for(int i=n-1;i>=0;i--){
+		for(int j=m-1;j>=0;j--){
+			printf("%d\t", arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}

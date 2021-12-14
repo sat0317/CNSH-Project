9e6a0c3dff62b1da81a8d06548bcb433776abad4
Yangsa <sat0317@naver.com>
2021-12-14 14:19:25 +0900

    Create CN.cpp

---
+++ 2020 ComputingChallenge/CN.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int n, a[100];
+    scanf("%d", &n);
+    for(int i=0;i<n/2;i++){
+    	int i1, i2;
+    	scanf("%d %d", &i1, &i2);
+    	printf("%d ", max(i1, i2));
+	}
+    
+    return 0;
+}
+

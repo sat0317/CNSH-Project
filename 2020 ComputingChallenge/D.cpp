15d97df4a686608171f41958cf4efb435872bd2b
Yangsa <sat0317@naver.com>
2021-12-27 08:43:10 +0900

    Create D.cpp

---
+++ 2020 ComputingChallenge/D.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    printf("%d %d %d", a, a, a);
+    
+    return 0;
+}
+

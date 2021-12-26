93d8ab98e96bd961affd9071244f4e8f2613d4ce
Yangsa <sat0317@naver.com>
2021-12-27 08:43:03 +0900

    Create CY.cpp

---
+++ 2020 ComputingChallenge/CY.cpp
@@ -0,0 +1,9 @@
+bool zero(int k)
+{
+  return !k; //논리 연산자 !는 false(0)를 true(1)로, true(1)를 false(0)으로 만든다.
+}
+
+bool plus(int k)
+{
+ return k>0; //k>0를 만족하는 경우에는 true(1)를, 아닌 경우에는 false(0)를 리턴한다.
+}

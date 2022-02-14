fe0e9378fa071ba6bfd0aae8e0db19046a908bc6
Yangsa <sat0317@naver.com>
2022-02-15 01:17:17 +0900

    Create E.cpp

---
+++ 2020 ComputingChallenge/E.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int a, b;
+    scanf("%d:%d", &a, &b);
+    printf("%d:%d", a, b);
+    
+    return 0;
+}
+

316c194db79c9bda070dce21f1e365dab2c8e4b8
Yangsa <sat0317@naver.com>
2022-02-21 03:35:59 +0900

    Create N.cpp

---
+++ 2020 ComputingChallenge/N.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    unsigned int n;
+scanf("%u", &n);
+printf("%u", n);
+    
+    return 0;
+}
+

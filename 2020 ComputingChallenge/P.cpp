a454b2d0afed234bc7f8b6fc383915b9e2ccf8a6
Yangsa <sat0317@naver.com>
2022-02-21 03:36:01 +0900

    Create P.cpp

---
+++ 2020 ComputingChallenge/P.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+	int a;
+	scanf("%d", &a);
+	printf("%X", a);
+	
+    return 0;
+}
+

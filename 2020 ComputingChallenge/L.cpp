596f8519103fc59c62687caa387fcbf276769f60
Yangsa <sat0317@naver.com>
2022-02-21 03:35:53 +0900

    Create L.cpp

---
+++ 2020 ComputingChallenge/L.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <memory.h>
+#define max(a, b) ((a)>(b)?(a):(b))
+#define min(a, b) ((a)<(b)?(a):(b))
+
+int main(){
+    int h, m, s;
+scanf("%d:%d:%d", &h, &m, &s);
+    printf("%d", m);
+    return 0;
+}
+
